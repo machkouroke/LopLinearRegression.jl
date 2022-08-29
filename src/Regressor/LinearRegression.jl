e72f06316201ca33371f689df6397554abe3822f
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-08-29 19:58:02 +0100

    feat: Add of accuracy score based on r^2 score

--- src/Regressor/LinearRegression.jl
+++ src/Regressor/LinearRegression.jl
@@ -1,4 +1,5 @@
 using Random: seed!, TaskLocalRNG
+using Statistics
 include("utilities.jl")
 mutable struct LinearRegression
 	a::Matrix{Float64}
@@ -12,19 +13,29 @@ mutable struct LinearRegression
         new(a, b, α, seed_value)
     end
 end
-function gradient!(regressor::LinearRegression, X_train, y_train, iter::Int64)::Array{Float32}
+function gradient!(regressor::LinearRegression, X_train, y_train, iter::Int64)::Tuple{Array{Float32}, Array{Float32}}
 	loss_train::Array{Float32} = []
-    accuracy = []
+    accuracy_train::Array{Float32} = []
     println("Start of gradient")
 	for i in 1:iter
 		F = f(regressor.a, regressor.b, X_train)
 		da, db = ∂a(X_train, y_train, F), ∂b(y_train, F)
 		regressor.a, regressor.b = update(regressor.a, regressor.b, da, db, regressor.α)
 		push!(loss_train, loss(y_train, X_train, regressor.a, regressor.b))
+        push!(accuracy_train, accuracy(regressor, X_train, y_train))
 	end
-	return loss_train
+	return loss_train, accuracy_train
 end
-function fit!(regressor::LinearRegression, X_train, y_train; iter::Int64=100)
-	loss_train = gradient!(regressor, X_train, y_train, 100)
-	return loss_train
+function fit!(regressor::LinearRegression, X_train, y_train; iter::Int64=100)::Tuple{Array, Array}
+	loss_train, accuracy = gradient!(regressor, X_train, y_train, 100)
+	return loss_train, accuracy
 end
+function predict(regressor::LinearRegression, X_test)::Array{Float32}
+    return f(regressor.a, regressor.b, X_test)
+end
+function accuracy(regressor::LinearRegression, X, y)::Float64
+    predict_sum = sum((y .- predict(regressor, X)).^2)
+    mean_sum = sum((y .- mean(y)).^2)
+    return 1 - (predict_sum / mean_sum)
+end
+
